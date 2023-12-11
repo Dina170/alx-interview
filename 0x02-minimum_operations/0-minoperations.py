8816ebf0033fa8c4e0e885c332bd87644a5c6946
dina <dinaelhadidy966@gmail.com>
2023-12-11 05:48:08 -0500

    Upload task

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""In a text file, there is a single character H.
+   Your text editor can execute only two operations in this file:
+   Copy All and Paste. Given a number n,
+   write a method that calculates the fewest number of operations
+   needed to result in exactly n H characters in the file.
+
+   Prototype: def minOperations(n)
+   Returns an integer
+   If n is impossible to achieve, return 0
+"""
+
+
+def minOperations(n):
+    """Returns the fewest number of operations needed to
+       result in exactly n H characters in the file
+    """
+    if type(n) is not int:
+        return 0
+    res = 0
+    for i in range(2, n):
+        while n % i == 0:
+            res += i
+            n = n / i
+
+    return res
