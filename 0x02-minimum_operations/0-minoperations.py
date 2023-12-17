15193e2d9d4762a4a8a76427b6c8dcebdd2fc6ab
dina <dinaelhadidy966@gmail.com>
2023-12-17 09:16:51 -0500

    Optmize solution

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -18,8 +18,14 @@ def minOperations(n):
     if type(n) is not int or n < 2:
         return 0
     res = 0
-    for i in range(2, n + 1):
+    i = 2
+    while i * i <= n:
         while n % i == 0:
             res += i
             n //= i
+        i += 1
+
+    if n > 1:
+        res += n
+
     return res
