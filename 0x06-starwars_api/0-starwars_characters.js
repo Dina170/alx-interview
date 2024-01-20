6c15578fa07dfc21f52d2d0a43422a31b5e39fec
dina <dinaelhadidy966@gmail.com>
2024-01-20 10:51:18 -0500

    Add async and await

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,14 +1,22 @@
 #!/usr/bin/node
+
 const request = require('request');
 
 const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
-request(url, (err, res, body) => {
-  if (err) throw err;
+
+request(url, async function (err, res, body) {
+  if (err) return console.error(err);
+
   const characters = JSON.parse(body).characters;
-  characters.forEach((character) => {
-    request(character, (_err, _res, _body) => {
-      if (_err) throw _err;
-      console.log(JSON.parse(_body).name);
+
+  for (const char of characters) {
+    await new Promise(function (resolve, reject) {
+      request(char, function (err, res, body) {
+        if (err) return console.error(err);
+
+        console.log(JSON.parse(body).name);
+        resolve();
+      });
     });
-  });
+  }
 });
